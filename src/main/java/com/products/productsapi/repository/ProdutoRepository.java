aac4e2b5f6d90fad291865a573546c3abc4869b7
jonatas <jonatas_dev@hotmail.com>
2025-06-13 17:41:13 -0300

    Refatora repository.

--- src/main/java/com/products/productsapi/repository/ProdutoRepository.java
+++ src/main/java/com/products/productsapi/repository/ProdutoRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProdutoRepository extends JpaRepository<ProdutoModel, String> {
 
-    //No generics<> passamos o nome da classe da entidade e o tipo do id.
+
 }
